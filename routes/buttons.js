fd2adc4270809e5595040d57ed8a851165a2506f
raspi-chromecast-box <raspiccbox03@gmail.com>
2020-02-29 20:30:08 -0500

    4

--- routes/buttons.js
+++ routes/buttons.js
@@ -3,7 +3,18 @@ const router = express.Router();
 const controller = require( "../controllers/buttons.js" );
 
 router.post( "/" , controller.press );
-router.get( "/1" , controller.press_1 )
+router.get( "/1" , controller.press_1 );
+router.get( "/2" , controller.press_2 );
+router.get( "/3" , controller.press_3 );
+router.get( "/4" , controller.press_4 );
+router.get( "/5" , controller.press_5 );
+router.get( "/6" , controller.press_6 );
+router.get( "/7" , controller.press_7 );
+router.get( "/8" , controller.press_8 );
+router.get( "/9" , controller.press_9 );
+router.get( "/10" , controller.press_10 );
+router.get( "/11" , controller.press_11 );
+router.get( "/12" , controller.press_12 );
 // router.put( "/status" , controller.put_status );
 // router.delete( "/status" , controller.delete_status );
 

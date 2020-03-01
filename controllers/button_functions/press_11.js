906d55750bcc9d9723a192e21e91ac26f696ca07
raspi-chromecast-box <raspiccbox03@gmail.com>
2020-03-01 14:10:59 -0500

    13

---
+++ controllers/button_functions/press_11.js
@@ -0,0 +1,24 @@
+const child = require( "child_process" );
+const exec = child.execSync;
+
+function EXEC( command ) {
+    try {
+        let result;
+        try { result = exec( command ); }
+        catch( error ) { console.log( error ); return false; }
+        if ( result ) {
+            result = result.toString();
+            if ( result ) {
+                result = result.trim();
+                console.log( result );
+            }
+        }
+        return true;
+    }
+    catch( error ) { console.log( error ); return false; }
+};
+
+function PRESS_BUTTON_11() {
+    EXEC( "/home/morphs/WORKSPACE/NODE/Commands/Spotify/Play.py --uri 'spotify:playlist:4PYhhcYPgUi9LXU9uiEATe'" )
+}
+module.exports = PRESS_BUTTON_11;
